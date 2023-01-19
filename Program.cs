35f7f0db3f4645ed0c8407e16b4bf1eb399f6938
SkulltechStuff <rt652@student.aru.ac.uk>
2023-01-19 15:54:04 +0000

    Added Division

--- Program.cs
+++ Program.cs
@@ -3,14 +3,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        int number1 = 7;
-        int number2 = 12;
+        double number1 = 7;
+        double number2 = 12;
         Console.Write("Enter the first number: ");
-        number1 = Convert.ToInt32( Console.ReadLine() );
+        number1 = Convert.ToDouble( Console.ReadLine() );
         Console.Write("Enter the second number: ");
-        number2 = Convert.ToInt32( Console.ReadLine() );
+        number2 = Convert.ToDouble( Console.ReadLine() );
         
         Console.WriteLine($"The sum of {number1} and {number2} is {number1+number2}");
+        Console.WriteLine($"{number1} divided by {number2} is {number1/number2}");
         Console.Write("Press any key to close program");
         Console.ReadKey(true);
     }
