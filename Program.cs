92ccd433dc1affe37fb392fb998174091a44efed
SkulltechStuff <rt652@student.aru.ac.uk>
2023-01-19 15:44:54 +0000

    Allows user to enter integers

--- Program.cs
+++ Program.cs
@@ -5,8 +5,13 @@ class Program
     {
         int number1 = 7;
         int number2 = 12;
-
+        Console.Write("Enter the first number: ");
+        number1 = Convert.ToInt32( Console.ReadLine() );
+        Console.Write("Enter the second number: ");
+        number2 = Convert.ToInt32( Console.ReadLine() );
+        
         Console.WriteLine($"The sum of {number1} and {number2} is {number1+number2}");
+        Console.Write("Press any key to close program");
         Console.ReadKey(true);
     }
 }
