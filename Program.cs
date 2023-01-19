a282fb647ff92dc90850d5eba8804bb467078ff2
SkulltechStuff <rt652@student.aru.ac.uk>
2023-01-19 15:27:42 +0000

    Changed numbers to variables

--- Program.cs
+++ Program.cs
@@ -3,7 +3,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine(7+12);
+        int number1 = 7;
+        int number2 = 12;
+        
+        Console.WriteLine(number1+number2);
         Console.ReadKey(true);
     }
 }
