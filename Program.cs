1d5505f2e3b3051f769ee8ac522b1ec45d610cb8
SkulltechStuff <rt652@student.aru.ac.uk>
2023-01-26 14:05:57 +0000

    Added a second method for checking if one number is bigger than the other

--- Program.cs
+++ Program.cs
@@ -3,9 +3,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        numbersSum();
+        conditions();
+        Console.Write("Press any key to close program");
+        Console.ReadKey(true);
     }
 
+    static void conditions()
+    {
+        //Check if x is larger than y
+        int x = 20;
+        int y = 20;
+
+        if(x > y)
+        {
+            Console.WriteLine("x is greater than y");
+        }
+        else if(y > x)
+        {
+            Console.WriteLine("y is greater than x");
+        }
+        else
+        {
+            Console.WriteLine("x and y are equal");
+        }
+    }
     static void numbersSum()
     {
         double number1 = 7;
@@ -16,8 +37,6 @@ class Program
         number2 = Convert.ToDouble( Console.ReadLine() );
         
         Console.WriteLine($"The sum of {number1} and {number2} is {number1+number2}");
-        Console.WriteLine($"{number1} divided by {number2} is {number1/number2}");
-        Console.Write("Press any key to close program");
-        Console.ReadKey(true);   
+        Console.WriteLine($"{number1} divided by {number2} is {number1/number2}"); 
     }
 }
