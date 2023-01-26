cd94e8d4c40b8fdcb8c15bcd9929e11863cfd4f4
SkulltechStuff <rt652@student.aru.ac.uk>
2023-01-26 14:59:07 +0000

    Added a third method that determines what age group the user is placed in

--- Program.cs
+++ Program.cs
@@ -3,11 +3,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        conditions();
+        ageCalculator();
         Console.Write("Press any key to close program");
         Console.ReadKey(true);
     }
 
+    static void ageCalculator()
+    {
+        string name = ("John");
+        int birthYear = 1999;
+        string birthday = ("No");
+        int age = 0;
+        int year = DateTime.Now.Year;
+        Console.WriteLine(year);
+
+        Console.Write("Enter your name: ");
+        name = Console.ReadLine();
+        Console.Write("Enter your year of birth: ");
+        birthYear = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Have you had your birthday this year? ");
+        birthday = Console.ReadLine().ToLower();
+
+        if(birthday == "yes" || birthday =="y")
+        {
+            age = (year-birthYear);
+        }
+        else
+        {
+            age = (year-birthYear-1);
+        }
+        if(age<18)
+        {
+            Console.WriteLine("You are a junior, " + name);
+        }
+        else if(age >=18 && age <30)
+        {
+            Console.WriteLine("You are a young adult, " + name);
+        }
+        else if(age >=30 && age <60)
+        {
+            Console.WriteLine("You are an adult, " + name);
+        }
+        else
+        {
+            Console.WriteLine("You are a senior, " + name);
+        }
+    }
     static void conditions()
     {
         //Check if x is larger than y
